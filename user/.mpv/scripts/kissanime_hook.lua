0b1dbae5adc802189ca480c856c7ac2866f27f28
siikamiika <siikamiika@users.noreply.github.com>
2015-11-29 04:38:01 +0200

    kissanime_hook.lua: change domain

--- user/.mpv/scripts/kissanime_hook.lua
+++ user/.mpv/scripts/kissanime_hook.lua
@@ -3,7 +3,7 @@ local utils = require('mp.utils')
 mp.add_hook("on_load", 9, function()
     local url = mp.get_property("stream-open-filename")
 
-    if (url:find('http://kissanime.com') == 1) then
+    if (url:find('https://kissanime.to') == 1) then
         local url = utils.subprocess({args={'kissanime-stream-url', url}}).stdout
         mp.set_property('stream-open-filename', url)
     end
